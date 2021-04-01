46b86e3a7b1ab1756bfb4c00e6a94cc64b7337a8
7wataaa <nn.contact.n@gmail.com>
2021-04-01 23:08:28 +0900

    init

---
+++ src/main.py
@@ -0,0 +1,36 @@
+import sys
+
+
+def is_natural_number(s):
+    try:
+        int(float(s))
+    except ValueError:
+        return False
+    else:
+        if int(float(s)) == 0:
+            return False
+        else:
+            return True
+
+
+args = sys.argv
+
+if len(args) != 2:
+    print('引数の数が不正です')
+    sys.exit()
+
+if (not is_natural_number(args[1])):
+    print('引数が自然数ではありません')
+    sys.exit()
+
+rng = int(float(args[1]))
+
+for i in range(1, rng+1):
+    if i % 15 == 0:
+        print('Fizz Buzz!!!')
+    elif i % 5 == 0:
+        print('Buzz!')
+    elif i % 3 == 0:
+        print('Fizz!')
+    else:
+        print(i)
